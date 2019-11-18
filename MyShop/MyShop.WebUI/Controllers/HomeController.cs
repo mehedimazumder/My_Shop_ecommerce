a89c0cfaae8f609bf8336d262e6382c3bdc9c409
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-19 00:20:38 +0600

    Product Listing adding viewModel

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 
 namespace MyShop.WebUI.Controllers
 {
@@ -18,10 +19,24 @@ namespace MyShop.WebUI.Controllers
             _context = productContext;
             _productCategories = productCategoriesContext;
         }
-        public ActionResult Index()
+        public ActionResult Index(string category = null)
         {
-            List<Product> products = _context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = _productCategories.Collection().ToList();
+
+            if (category == null)
+            {
+                products = _context.Collection().ToList();
+            }
+            else
+            {
+                products = _context.Collection().Where(p => p.Category == category).ToList();
+            }
+            ProductListViewModel viewModel = new ProductListViewModel();
+            viewModel.Products = products;
+            viewModel.ProductCategories = categories;
+
+            return View(viewModel);
         }
 
         public ActionResult Details(string id)
